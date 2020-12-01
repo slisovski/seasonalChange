8535322ff9851cd7718c3f365d42ab46c7e4bc2b
Simeon Lisovski <simeon.lisovski@vogelwarte.ch>
2020-12-01 09:41:53 +0100

    phenBatches

--- Scripts/phenBatches.R
+++ Scripts/phenBatches.R
@@ -2,6 +2,9 @@ library(sf)
 library(raster)
 library(rgdal)
 library(rgeos)
+library(parallel)
+library(biwavelet)
+source('~/Documents/GitHub/seasonalChange/Scripts/functions.R', echo=FALSE)
 
 ## land mask
 lake <- read_sf("Data/GeoDat/ne_50m_lakes/ne_50m_lakes.shp")
@@ -16,6 +19,7 @@ pol <- rasterToPolygons(r0)
 
 ## batches
 batch_dir <- "/Users/slisovsk/Desktop/batches/"
+phen_dir  <- "/Users/slisovsk/Desktop/phenBatches/"
 batches   <- list.files(batch_dir, pattern = ".rda")
 batchID   <- sapply(strsplit(batches, "_"), function(x) as.numeric(substring(x[[2]], 1, nchar(x[[2]]) -4)))
 load("Results/dateSequence.rda") 
@@ -29,135 +33,221 @@ plot(pol, add = T)
 centr <- do.call("rbind", lapply(1:length(pol), function(x) coordinates(gCentroid(pol[x,]))))
 text(centr[batchID,1], centr[batchID,2], batchID, cex = 0.4)
 
-# for(batch in batchID) {
-  
-  batch <- 613
+for(batch in batchID) {
 
-  load(paste0(batch_dir, "batch_", batch, ".rda"))
-  
-  onLand  <- apply(outBatch$crds[,-c(1:3, ncol(outBatch$crds))], 1, any) & 
-    !outBatch$crds[,ncol(outBatch$crds)] & 
-    outBatch$crds[,3]
-  
-  inBatch <- apply(outBatch$crds[,-c(1:3, ncol(outBatch$crds))], 1, any) & 
-    !outBatch$crds[,ncol(outBatch$crds)]
-  
-  st_crds  <- st_sfc(lapply(1:nrow(outBatch$crds), function(x) st_point(as.numeric(outBatch$crds[x,1:2]))), crs = 4326)
-  
-  # test <- rasterFromXYZ(cbind(outBatch$crds[,1:2], outBatch$dat[,420,1]))
-  # plot(test, breaks = seq(0,0.8, length = 100), col = terrain.colors(100))
-  # plot(land$geometry, add = T, col = NA)
-  # plot(lake$geometry, add = T)
-  # points(outBatch$crds[inBatch,1:2], cex = 0.3, pch = 16, col = "black")
-  # points(outBatch$crds[onLand,1:2], cex = 0.3, pch = 16, col = "red")
+load(paste0(batch_dir, "batch_", batch, ".rda"))
 
-  coordinates_dt <- optiRum::CJ.dt(data.table::as.data.table(outBatch$crds[inBatch,1:2]),
-                                   data.table::as.data.table(outBatch$crds[inBatch,1:2]))
-  
-  distM <- matrix(with(coordinates_dt, spatialrisk::haversine(y, x, i.y, i.x)), 
-                  ncol = sum(inBatch), nrow = sum(inBatch))
+onLand  <- apply(outBatch$crds[,-c(1:3, ncol(outBatch$crds))], 1, any) &
+  !outBatch$crds[,ncol(outBatch$crds)] &
+  outBatch$crds[,3]
 
+inBatch <- apply(outBatch$crds[,-c(1:3, ncol(outBatch$crds))], 1, any) &
+  !outBatch$crds[,ncol(outBatch$crds)]
+
+st_crds  <- st_sfc(lapply(1:nrow(outBatch$crds), function(x) st_point(as.numeric(outBatch$crds[x,1:2]))), crs = 4326)
+
+# test <- rasterFromXYZ(cbind(outBatch$crds[,1:2], outBatch$dat[,420,1]))
+# plot(test, breaks = seq(0,0.8, length = 100), col = terrain.colors(100))
+# plot(land$geometry, add = T, col = NA)
+# plot(lake$geometry, add = T)
+# points(outBatch$crds[inBatch,1:2], cex = 0.3, pch = 16, col = "black")
+# points(outBatch$crds[onLand,1:2], cex = 0.3, pch = 16, col = "red")
+
+coordinates_dt <- optiRum::CJ.dt(data.table::as.data.table(outBatch$crds[inBatch,1:2]),
+                                 data.table::as.data.table(outBatch$crds[inBatch,1:2]))
+
+distM <- matrix(with(coordinates_dt, spatialrisk::haversine(y, x, i.y, i.x)), 
+                ncol = sum(inBatch), nrow = sum(inBatch))
+
+
+pxlPhen <- mclapply(which(onLand), function(pxl) {
   
-  pxlPhen <- mclapply(which(onLand), function(pxl) {
+  # pxl <- which(onLand)[11000:11010][2]
+  
+  inbfr  <- which(inBatch)[which(distM[which(inBatch)==pxl,]<15000)]
+
+  # points(outBatch$crds[pxl, 1], outBatch$crds[pxl, 2])
+  # points(outBatch$crds[inbfr, 1:2], pch = 16, cex = 0.5, col = "magenta")
+
+  dst    <- distM[which(inBatch)==pxl, which(inBatch)%in%inbfr]/1000
+  weigth <- approx(c(0, max(dnorm(dst, 0, 4))), c(0,1), dnorm(dst, 0, 4))$y
+
+  dat    <- outBatch$dat[inbfr,,]
+  evi    <- ifelse(!is.na(dat[,,2]) | dat[,,1] < -0.1 , NA, dat[,,1])
+
+  # matplot(dates, t(evi), type= "o", lty = 1, lwd = 1, col = adjustcolor("darkgreen", alpha.f = 0.5), pch = 16)
+  
+  med <- apply(evi, 2, median , na.rm = T)
+  
+  if(diff(quantile(med, prob = c(0.025,0.975), na.rm = T))>0.1) { ## diff ts > 0.1
     
-      inbfr  <- which(inBatch)[which(distM[which(inBatch)==pxl,]<15000)]
+    wt <- biwavelet::wt(cbind(1:length(med), zoo::na.approx(med)))
+    power  <- log2(wt$power.corr)
+    per    <- wt$period[which.max(apply(power, 1, median, na.rm = T))]
+
+    if(round(per,0)%in%c(42:62)) {
+      period = 1
+    } else {
+      if(round(wt$period[which.max(apply(power, 1, median))],0)%in%c(16:36)) {
+        period = 0.5
+      } else period = NA
+    }
+    
+    datCurve <- merge(data.frame(year = as.numeric(format(seq(min(dates), max(dates), by = "week"), "%Y")),
+                                 week = as.numeric(format(seq(min(dates), max(dates), by = "week"), "%U"))),
+                      data.frame(year = as.numeric(format(dates, "%Y")), week = as.numeric(format(dates, "%U")),
+                                 date = dates,
+                                 evi  = med, t(evi)), all.x = T)
       
-      # points(outBatch$crds[pxl, 1], outBatch$crds[pxl, 2])
-      # points(outBatch$crds[inbfr, 1:2], pch = 16, cex = 0.5, col = "magenta")
+    if(period == 1) {
+      fit0  <- optim(fn = lsCos, par = c(a = 1, b = 0), f = 52, Mx = zoo::na.approx(datCurve$evi) - median(datCurve$evi, na.rm = T), sd = 0.01)
+      curve <- fit0$par[1]*cos(pi*((1:length(datCurve$evi))/(length(datCurve$evi)/((length(datCurve$evi)/52)*2))) +
+                                 (pi+fit0$par[2])) +  mean(med, na.rm=T)
+    } else{
+      fit0  <- optim(fn = lsCos, par = c(a = 1, b = 0), f = 26, Mx = zoo::na.approx(datCurve$evi) - median(datCurve$evi, na.rm = T), sd = 0.01)
+      curve <- fit0$par[1]*cos(pi*((1:length(datCurve$evi))/(length(datCurve$evi)/((length(datCurve$evi)/26)*2))) +
+                                 (pi+fit0$par[2])) +  mean(med, na.rm=T)
+    }
+    
+   # plot( datCurve$evi, type = "o", pch = 16, cex = 0.5, col = "darkgreen")
+   # par(new = T)
+   # plot(curve, type = "l", col = "orange")
+    
+    mins <- unique(c(1, which(diff(sign(diff(-curve)))==-2)+1, length(curve)))
+    maxs <- which(diff(sign(diff( curve)))==-2)+1
+    
+    segL <- apply(do.call("rbind", lapply(maxs, function(x) mins[c(1:length(mins))[order(abs(x-mins))][1:2]]+c(-10,10))), 1, 
+                  function(x) datCurve[ifelse(x[1]<1, 1, x[1]):ifelse(x[2]>length(curve), length(curve), x[2]),])
+    
+    q50 <- median(sapply(segL, function(x) quantile(x[,4], prob = 0.5, na.rm = T)), na.rm = T)
+    
+    phen0 <- do.call("rbind", lapply(segL, function(s) {
       
-      dst    <- distM[pxl,inbfr]/1000
-      weigth <- approx(c(0, max(dnorm(dst, 0, 5))), c(0,1), dnorm(dst, 0, 5))$y
+      # s <- segL[[77]]
+      # matplot(s$date, s[,-c(1:4)], type= "l", lty = 1, lwd = 1, col = adjustcolor("darkgreen", alpha.f = 0.5), pch = 16)
+      # lines(s$date, s[,-c(1:4)][,weigth==1], lwd = 4, col = "orange")
+
+      dateSeg <- zoo::na.approx(s$date)
       
-      dat    <- outBatch$dat[inbfr,,]
-      evi    <- ifelse(!is.na(dat[,,2]) | dat[,,1] < -0.1 , NA, dat[,,1])
+      if(length(dateSeg)>20) {
       
-  
-      ### segments
-      med <- apply(evi, 2, median , na.rm = T)
-      # matplot(dates, t(evi), type= "o", lty = 1, lwd = 1, col = adjustcolor("darkgreen", alpha.f = 0.5), pch = 16)
+      tmpDat <- subset(data.frame(x = rep(s$date, ncol(s)-4),
+                                  y = unlist(t(c(s[,-c(1:4)]))),
+                                  w = rep(weigth, each = nrow(s))), !is.na(y))
       
-      if(sum(!is.na(med))>(length(med)*0.75)) {    
-          
-          q50 <- quantile(evi, prob = 0.5, na.rm = T)
-          
-          peaks   <- FindPeaks(med)
-          pars    <- list(rel_amp_frac = 0.25, rel_peak_frac = 0.1, min_seg_amplitude = 0.15)
-          
-          segs    <- do.call("rbind", GetSegs(peaks, med, pars))
-          segs    <- segs[order(segs[,1]),]
-          
-          # plot(dates, med, type = "o")
-          # apply(segs, 1, function(x) rect(dates[x[1]], 0, dates[x[3]], 1, col = adjustcolor("grey90", alpha.f = 0.2), border =  "grey10"))
-          # abline(v = dates[segs[,2]], col = "orange", lty = 2)
-          
-          segL    <- lapply(1:nrow(segs), function(x) evi[,segs[x,1]:segs[x,3]])  
-          dtsS    <- lapply(1:nrow(segs), function(x) dates[segs[x,1]:segs[x,3]])  
-          
-          phen <- do.call("rbind", lapply(1:nrow(segs), function(s) {
-          
-                # matplot(dtsS[[s]], t(segL[[s]]), type= "o", lty = 1, lwd = 1, col = adjustcolor("darkgreen", alpha.f = 0.5), pch = 16)
-                tmpDat <- data.frame(x = rep(dtsS[[s]], each = nrow(segL[[s]])),
-                                     y = c(segL[[s]]),
-                                     w = rep(weigth, each = ncol(segL[[s]])))    
+      spl     <- smooth.spline(x = tmpDat$x, y = tmpDat$y, spar = 0.3, w = tmpDat$w)
+      xSmooth <- predict(spl, dateSeg)$y
+      peaks   <- FindPeaks(xSmooth)
+      
+      # lines(dateSeg, xSmooth, lwd = 6, col = "orange")
+      # abline(v = dateSeg[peaks], lty = 2, col = "grey80")
+      
+      pars    <- list(rel_amp_frac = 0.15, rel_peak_frac = 0.1, min_seg_amplitude = 0.1)
+      segs    <- tryCatch(do.call("rbind", GetSegs(peaks, xSmooth, pars)), error = function(e) NULL)
+      
+      # apply(segs, 1, function(x) rect(dateSeg[x[1]], 0, dateSeg[x[3]], 1, col = adjustcolor("grey80", alpha.f = 0.2), border =  "grey10"))
+      
+      year   <- median(as.numeric(format(s$date, "%Y")), na.rm = T)
+       
+      if(!is.null(segs)) {      
+                 
+            if(nrow(segs)>1) {
+              
+              max_in  <- min(segs[order(xSmooth[segs[,2]], decreasing = T),2][1:2])
+              max_out <- max(segs[order(xSmooth[segs[,2]], decreasing = T),2][1:2])
+              
+              seqRan  <- c(min(segs[order(xSmooth[segs[,2]], decreasing = T),1]),
+                           max(segs[order(xSmooth[segs[,2]], decreasing = T),3]))
+                
+            } else {
+              
+              max_in <- max_out  <- segs[2]
+              seqRan <- segs[c(1,3)]
+              
+            }
+        
+            max    <- as.numeric(format(as.POSIXct(dateSeg, origin = "1970-01-01")[max_in], "%j"))
+            amp    <- diff(range(xSmooth[seqRan[1]:seqRan[2]]))
             
-                spl <- with(tmpDat[!is.na(tmpDat$y),], smooth.spline(x, y, spar = 0.3, w = w))
-                xSmooth <- predict(spl, as.numeric(dtsS[[s]]))$y
-        
-                spline  <- data.frame(ind = 1:length(dtsS[[s]]), d = dtsS[[s]], evi = xSmooth, 
-                                      gup = ifelse(dtsS[[s]]<=dates[segs[s,2]], TRUE, FALSE))
-                
-                # lines(dtsS[[s]], xSmooth, lwd = 2, col = "orange")
-                # abline(v = dates[segs[s,2]])
-                # abline(h = q50)
-                
-                year   <- median(as.numeric(format(dtsS[[s]], "%Y")))
-                
-                max    <- as.numeric(format(dates[segs[s,2]], "%j"))
-                maxInd <- which(spline$d==dates[segs[s,2]])
-                amp    <- diff(range(xSmooth))
-        
-                  
-                q10gup <- with(spline[spline$gup,],  rev(ind)[GetThresh(min(evi) + diff(range(evi))*0.1, rev(evi), gup = F, first_greater = T)])
-                q10sen <- with(spline[!spline$gup,], rev(ind)[GetThresh(min(evi) + diff(range(evi))*0.1, rev(evi), gup = T, first_greater = F)])
-                
-                q50gup <- with(spline[spline$gup,],  rev(ind)[GetThresh(q50, rev(evi), gup = F, first_greater = T)])
-                q50sen <- with(spline[!spline$gup,], rev(ind)[GetThresh(q50, rev(evi), gup = T, first_greater = F)])
-                
-                area   <- with(spline[spline$ind>=q10gup & spline$ind<=q10sen,],
-                               MESS:::auc(ind, evi, type = 'spline'))
-                
-                c(year, max, amp, area, abs(c(q10gup, q50gup, q10sen, q50sen)-maxInd))
-
-        }))
-    
-          phenOut <- merge(data.frame(year = 1981:2020), data.frame(year = phen[,1],
-                                                                    max  = phen[,2],
-                                                                    amp  = phen[,3],
-                                                                    area = phen[,4],
-                                                                    q10gup = phen[,5],
-                                                                    q50gub = phen[,6],
-                                                                    q10sen = phen[,7],
-                                                                    q50sen = phen[,8]), all.x = T)
-  
-      }  else {
-        
-         data.frame(year = 1981:2020, 
-                    max  = NA, 
-                    amp  = NA, area = NA, 
-                    q10gup = NA, q50gub = NA, 
-                    q10sen = NA, q50sen = NA)
+            
+            q10gup <- with(data.frame(t = dateSeg, y = xSmooth)[seqRan[1]:max_in,],  rev(t)[GetThresh(min(y) + diff(range(y))*0.1, rev(y), gup = F, first_greater = T)])
+              # abline(v = q10gup)
+            q50gup <- with(data.frame(t = dateSeg, y = xSmooth)[seqRan[1]:max_in,],  rev(t)[GetThresh(q50, rev(y), gup = F, first_greater = T)])
+              # abline(v = q50gup)
+            
+            q10sen <- with(data.frame(t = dateSeg, y = xSmooth)[max_out:seqRan[2],],  rev(t)[GetThresh(min(y) + diff(range(y))*0.1, rev(y), gup = T, first_greater = F)])
+              # abline(v = q10sen)
+            q50sen <- with(data.frame(t = dateSeg, y = xSmooth)[max_out:seqRan[2],],  rev(t)[GetThresh(q50, rev(y), gup = T, first_greater = F)])
+              # abline(v = q50sen)
+            
+            area   <- with(data.frame(t = 1:length(dateSeg), y = xSmooth)[seqRan[1]:seqRan[2],],
+                           MESS:::auc(t, y, type = 'spline'))
+            
+            
+            out <- c(year = year, max = max, amp = round(amp, 2), area = round(area,2), 
+                     as.numeric(format(as.POSIXct(c(q10gup, q50gup, q10sen, q50sen), origin = "1970-01-01"), "%j")))
+              out[5:6] <- ifelse(out[5:6]>=max, out[5:6] - 365, out[5:6])
+              out[7:8] <- ifelse(out[7:8]<=max, 365 + out[7:8], out[7:8])
+            
+            out
+      } else {
+        c(year = year, max = NA, amp = NA, area = NA, rep(NA, 4))
       }
-  
-
       
-  }, mc.cores = 5)
-  
+      } else {
+        c(year = year, max = NA, amp = NA, area = NA, rep(NA, 4))
+      }
+    }))
+    
+    if(period==1) {
+      phen <- aggregate(phen0, by = list(phen0[,1]), median)[,-1]
+    } else {
+      pk   <- round(median(unlist(aggregate(phen0[,3], by = list(phen0[,1]), FUN = function(a) which.max(a))$x), na.rm = T), 0)
+      phen <- do.call("rbind", lapply(split(as.data.frame(phen0), round(phen0[,1],0)), function(a) {
+        a[1,]
+      }))
+    }
 
-mx <- sapply(pxlPhen, function(x) median(x[,7], na.rm = T))
+    merge(data.frame(year = 1981:2020), data.frame(year = phen[,1],
+                                                   per  = round(per,0),
+                                                   max  = phen[,2],
+                                                   amp  = phen[,3],
+                                                   area = phen[,4],
+                                                   q10gup = phen[,5],
+                                                   q50gub = phen[,6],
+                                                   q10sen = phen[,7],
+                                                   q50sen = phen[,8]), all.x = T)[,-1]
+    
+    
+    
+  } else { #### diff ts range < 0.1
+    
+    data.frame(year = 1981:2020, 
+               per = NA,
+               max  = NA, 
+               amp  = NA, area = NA, 
+               q10gup = NA, q50gub = NA, 
+               q10sen = NA, q50sen = NA)[,-1]
+    
+  }
   
-test0   <- rasterFromXYZ(cbind(outBatch$crds[,1:2], outBatch$dat[,420,1]))
-test0[] <- NA
-test0[which(onLand)] <- mx
-  
-plot(test0)
-  
+}, mc.cores = max(5, detectCores()-10))
+
+## period, max, amp, area, q10gub, q50gub, q10sen, q50sen
+phenA <- abind::abind(lapply(1:ncol(pxlPhen[[1]]), function(l) matrix(unlist(sapply(pxlPhen, function(x) x[,l])), ncol = nrow(pxlPhen[[1]]), byrow = T)), along = 3)
+
+phenR <- rasterFromXYZ(cbind(outBatch$crds[outBatch$crds[,3],1:2], 
+                             (apply(outBatch$crds[,-c(1:3, ncol(outBatch$crds))], 1, any) &
+                             !outBatch$crds[,ncol(outBatch$crds)])[outBatch$crds[,3]]), crs = CRS("+proj=longlat"))
+names(phenR) <- paste0("batch_", batch)
+
+phenOut <- list(dat = phenA, raster = phenR)
+
+save(phenOut, file = paste0(phen_dir, "phenBatch_", batch, ".rda"))
+
+# rTest <- phenR
+# rTest[rTest[]==1] <- apply(phenOut[,,2], 1, median, na.rm = T)
+# 
+# plot(rTest)
+
+}
